a150fdb4b009071660527dbe78266a4972ff98ba
molly <mollynzembi@gmail.com>
2024-04-25 08:57:07 +0300

    errrors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,10 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>    {links.map(link =>(
+    <projectitem link ={link.usename}name={student.name}id={student.id}tm={student.tm}></projectitem>
+  ))}
+</nav>;
 }
 
 export default NavBar;
